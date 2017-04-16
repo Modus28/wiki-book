c7eb385849676ece5a52df1b2d9eb216ab20a233
Daniel Grigsby <dbg28@case.edu>
2017-04-16 07:39:20 -0400

    Merged html-to-text converter and database connector'

--- fbchat-master/wikipedia_to_database.py
+++ fbchat-master/wikipedia_to_database.py
@@ -1,6 +1,31 @@
 import urllib, urllib.request
 from bs4 import BeautifulSoup
+import psycopg2
 
+params = {
+  'dbname': 'd12u190iuusr3o',
+  'user': 'quszuctlcqieqr',
+  'password': '07351f585b5c9721034f3b7fa97c7d970a40a018d945a3b52e6a288fd7946cd8',
+  'host': 'ec2-174-129-227-116.compute-1.amazonaws.com',
+  'port': 5432
+}
+
+
+def doInsert(keyword,text) :
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO wiki_links VALUES ('" + keyword + "','" + text + "');")
+    conn.commit()
+    conn.close()
+	
+
+
+def doQuery(keyword) :
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    cur.execute( "SELECT text FROM wiki_links where keyword = '" + keyword + "';"  )
+    print(cur.fetchone())
+    conn.close()
 
 # Written by Adam Beck. Takes in a URL, and returns an acceptably formatted
 # string that contains all of the text from that webpage.
